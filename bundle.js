79456aab40a15f96da37dfb1c2a93e3f556ebf81
Doug Cole <idugcoal@gmail.com>
2018-07-13 10:14:42 -0700

    linting

--- bundle.js
+++ bundle.js
@@ -1,7 +1,5 @@
-
-let currentTurn = 0;
-const players = ['X', 'O'];
 const board = [...document.querySelectorAll('div.square')];
+const players = ['X', 'O'];
 const wins = [
   [0, 1, 2],
   [3, 4, 5],
@@ -12,14 +10,15 @@ const wins = [
   [0, 4, 8],
   [2, 4, 6],
 ];
+let currentTurn = 0;
 
 function isGameOver() {
   for (let i = 0; i < 8; i += 1) {
     const win = wins[i];
-    if (board[win[0]].innerText === players[currentTurn % 2]
-        && board[win[1]].innerText === players[currentTurn % 2]
-        && board[win[2]].innerText === players[currentTurn % 2]) {
-      console.log(players[currentTurn % 2], 'wins');
+    if (board[win[0]].innerText === players[(currentTurn % 2) + 1]
+        && board[win[1]].innerText === players[(currentTurn % 2) + 1]
+        && board[win[2]].innerText === players[(currentTurn % 2) + 1]) {
+      console.log(players[(currentTurn % 2) + 1], 'WINS!!!');
       return true;
     }
   }
@@ -30,34 +29,37 @@ function isGameOver() {
 function selectComputerMove() {
   for (let i = 0; i < 8; i += 1) {
     const win = wins[i];
-    console.log('1:', win[0], board[win[0]].innerText, '2:', win[1], board[win[1]].innerText, '3:', win[2], board[win[2]].innerText);
+    // console.log(win[0], board[win[0]].innerText, win[1], board[win[1]].innerText, win[2], board[win[2]].innerText);
     if (board[win[0]].innerText === 'O' && board[win[1]].innerText === 'O' && board[win[2]].innerText === '') {
       board[win[2]].innerText = 'O';
-      console.log('winner: ', win[2]);
+      console.log('win1 ', win[2]);
       break;
     } else if (board[win[0]].innerText === 'O' && board[win[2]].innerText === 'O' && board[win[1]].innerText === '') {
       board[win[1]].innerText = 'O';
-      console.log('2');
+      console.log('win2');
       break;
     } else if (board[win[1]].innerText === 'O' && board[win[2]].innerText === 'O' && board[win[0]].innerText === '') {
       board[win[0]].innerText = 'O';
-      console.log('3');
-      break;
-    } else if (board[win[0]].innerText === 'X' && board[win[1]].innerText === 'X' && board[win[2]].innerText === '') {
-      board[win[2]].innerText = 'O';
-      console.log('4');
-      break;
-    } else if (board[win[0]].innerText === 'X' && board[win[2]].innerText === 'X' && board[win[1]].innerText === '') {
-      board[win[1]].innerText = 'O';
-      console.log('5');
-      break;
-    } else if (board[win[1]].innerText === 'X' && board[win[2]].innerText === 'X' && board[win[0]].innerText === '') {
-      board[win[0]].innerText = 'O';
-      console.log('6');
+      console.log('win3');
       break;
     }
   }
-  console.log('after IFs', currentTurn);
+  for (let i = 0; i < 8; i += 1) {
+    const win = wins[i];
+    if (board[win[0]].innerText === 'X' && board[win[1]].innerText === 'X' && board[win[2]].innerText === '') {
+      board[win[2]].innerText = 'O';
+      console.log('win4');
+      break;
+    } else if (board[win[0]].innerText === 'X' && board[win[2]].innerText === 'X' && board[win[1]].innerText === '') {
+      board[win[1]].innerText = 'O';
+      console.log('win5');
+      break;
+    } else if (board[win[1]].innerText === 'X' && board[win[2]].innerText === 'X' && board[win[0]].innerText === '') {
+      board[win[0]].innerText = 'O';
+      console.log('win6');
+      break;
+    }
+  }
   if (currentTurn === 1) {
     if (board[4].innerText === '') {
       board[4].innerText = 'O';
@@ -66,23 +68,24 @@ function selectComputerMove() {
     }
   }
 
+  console.log('after IFs', currentTurn);
   currentTurn += 1;
   isGameOver();
 }
 
 function main() {
-  board.forEach(square => {
-    square.addEventListener('click', e => {
-      if(e.target.innerText === '') {
-        e.target.innerText = players[currentTurn % 2]
-        isGameOver()
-        currentTurn++
-        if(currentTurn % 2 === 1) {
-          selectComputerMove()
+  board.forEach((square) => {
+    square.addEventListener('click', (e) => {
+      if (e.target.innerText === '') {
+        e.target.innerText = players[currentTurn % 2];
+        isGameOver();
+        currentTurn += 1;
+        if (currentTurn % 2 === 1) {
+          selectComputerMove();
         }
       }
-    })
-  })
+    });
+  });
 }
 
 main();
