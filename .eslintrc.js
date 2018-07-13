718240a1dc23b05ef92a45371dcc02dcba131ba0
Doug Cole <idugcoal@gmail.com>
2018-07-13 09:56:07 -0700

    refactor

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
     "extends": "airbnb-base",
     "plugins": [
         "import"
-    ]
+    ],
+    "globals": {
+      "window": true,
+      "document": true
+    }
 };
--- bundle.js
+++ bundle.js
@@ -1,94 +1,84 @@
-/*eslint-disable */
+
 let currentTurn = 0;
+const players = ['X', 'O'];
+const board = [...document.querySelectorAll('div.square')];
+const wins = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
-function isGameOver(board, player) {
-  const wins = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  wins.forEach((win) => {
-    if (board[win[0]].innerText === player
-        && board[win[1]].innerText === player
-        && board[win[2]].innerText === player) {
-      console.log(player, 'wins');
+function isGameOver() {
+  for (let i = 0; i < 8; i += 1) {
+    const win = wins[i];
+    if (board[win[0]].innerText === players[currentTurn % 2]
+        && board[win[1]].innerText === players[currentTurn % 2]
+        && board[win[2]].innerText === players[currentTurn % 2]) {
+      console.log(players[currentTurn % 2], 'wins');
       return true;
     }
-  });
-
+  }
 
   return false;
 }
 
-function findPairs(board) {
-  const wins = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (const win of wins) {
-    //look for any empty pairs and place for win
-    console.log(board, win[0], win[1], win[2])
+function selectComputerMove() {
+  for (let i = 0; i < 8; i += 1) {
+    const win = wins[i];
+    console.log('1:', win[0], board[win[0]].innerText, '2:', win[1], board[win[1]].innerText, '3:', win[2], board[win[2]].innerText);
     if (board[win[0]].innerText === 'O' && board[win[1]].innerText === 'O' && board[win[2]].innerText === '') {
       board[win[2]].innerText = 'O';
-      currentTurn++
+      console.log('winner: ', win[2]);
+      break;
+    } else if (board[win[0]].innerText === 'O' && board[win[2]].innerText === 'O' && board[win[1]].innerText === '') {
+      board[win[1]].innerText = 'O';
+      console.log('2');
+      break;
+    } else if (board[win[1]].innerText === 'O' && board[win[2]].innerText === 'O' && board[win[0]].innerText === '') {
+      board[win[0]].innerText = 'O';
+      console.log('3');
+      break;
+    } else if (board[win[0]].innerText === 'X' && board[win[1]].innerText === 'X' && board[win[2]].innerText === '') {
+      board[win[2]].innerText = 'O';
+      console.log('4');
+      break;
+    } else if (board[win[0]].innerText === 'X' && board[win[2]].innerText === 'X' && board[win[1]].innerText === '') {
+      board[win[1]].innerText = 'O';
+      console.log('5');
+      break;
+    } else if (board[win[1]].innerText === 'X' && board[win[2]].innerText === 'X' && board[win[0]].innerText === '') {
+      board[win[0]].innerText = 'O';
+      console.log('6');
       break;
     }
-    else if (board[win[0]].innerText === 'O' && board[win[2]].innerText === 'O' && board[win[1]].innerText === '') {
-      board[win[1]].innerText = 'O'
-      currentTurn++
-      break
+  }
+  console.log('after IFs', currentTurn);
+  if (currentTurn === 1) {
+    if (board[4].innerText === '') {
+      board[4].innerText = 'O';
+    } else {
+      board[8].innerText = 'O';
     }
-    else if (board[win[1]].innerText === 'O' && board[win[2]].innerText === 'O' && board[win[0]].innerText === '') {
-      board[win[0]].innerText = 'O'
-      currentTurn++
-      break
-    }
-    //look for any other empty player pairs and place for block
-    else {
-      if(board[4].innerText === '') {
-        console.log('center')
-        board[4].innerText = 'O'
-        currentTurn++
-        break
-      } else {
-        console.log('corner')
-        board[8].innerText = 'O'
-        currentTurn++
-        break
-      }
-    }
-}
-}
+  }
 
-function selectComputerMove(board) {
-  findPairs(board);
+  currentTurn += 1;
+  isGameOver();
 }
 
 function main() {
-  const players = ['X', 'O']
-
-  const board = [...document.querySelectorAll("div.square")]
   board.forEach(square => {
     square.addEventListener('click', e => {
       if(e.target.innerText === '') {
         e.target.innerText = players[currentTurn % 2]
-        isGameOver(board, players[currentTurn % 2])
+        isGameOver()
         currentTurn++
         if(currentTurn % 2 === 1) {
-          selectComputerMove(board)
+          selectComputerMove()
         }
       }
     })
