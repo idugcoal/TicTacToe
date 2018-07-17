a8713a7b38406de6a5605bf3b68ef9e6734dd8ab
Doug Cole <idugcoal@gmail.com>
2018-07-17 11:19:45 -0700

    adjusts colors

--- bundle.js
+++ bundle.js
@@ -60,39 +60,11 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 3);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-var _game = __webpack_require__(5);
-
-var _state = __webpack_require__(15);
-
-var state = _interopRequireWildcard(_state);
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
-
-function main() {
-  state.winner.addEventListener('click', e => {
-    e.target.click = (0, _game.resetGame)(state);
-  });
-  state.board.forEach(square => {
-    square.addEventListener('click', e => {
-      (0, _game.playGame)(e);
-    });
-  });
-}
-
-main();
-
-/***/ }),
-/* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -117,34 +89,7 @@ function isGameOver(state) {
 }
 
 /***/ }),
-/* 2 */,
-/* 3 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = resetGame;
-function resetGame(state) {
-  state.currentTurn = 0;
-  state.winner.style.visibility = 'hidden';
-  state.board.forEach(square => {
-    const temp = square;
-    temp.innerText = '';
-  });
-  const winningSquares = document.querySelectorAll('div.square');
-  for (let i = 0; i < winningSquares.length; i += 1) {
-    winningSquares[i].classList.remove(`win-${state.winningCondition}`);
-  }
-  state.winningCondition = -1;
-  state.gameOverFlag = false;
-}
-
-/***/ }),
-/* 4 */
+/* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -155,7 +100,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = getComputerMove;
 
-var _helpers = __webpack_require__(9);
+var _helpers = __webpack_require__(8);
 
 function getComputerMove(state) {
   for (let i = 0; i < 8; i += 1) {
@@ -201,277 +146,7 @@ function getComputerMove(state) {
 }
 
 /***/ }),
-/* 5 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _playGame = __webpack_require__(6);
-
-Object.defineProperty(exports, 'playGame', {
-  enumerable: true,
-  get: function () {
-    return _interopRequireDefault(_playGame).default;
-  }
-});
-
-var _isGameOver = __webpack_require__(1);
-
-Object.defineProperty(exports, 'isGameOver', {
-  enumerable: true,
-  get: function () {
-    return _interopRequireDefault(_isGameOver).default;
-  }
-});
-
-var _getComputerMove = __webpack_require__(4);
-
-Object.defineProperty(exports, 'getComputerMove', {
-  enumerable: true,
-  get: function () {
-    return _interopRequireDefault(_getComputerMove).default;
-  }
-});
-
-var _resetGame = __webpack_require__(3);
-
-Object.defineProperty(exports, 'resetGame', {
-  enumerable: true,
-  get: function () {
-    return _interopRequireDefault(_resetGame).default;
-  }
-});
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-/***/ }),
-/* 6 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = playGame;
-
-var _isGameOver = __webpack_require__(1);
-
-var _isGameOver2 = _interopRequireDefault(_isGameOver);
-
-var _showResult = __webpack_require__(17);
-
-var _showResult2 = _interopRequireDefault(_showResult);
-
-var _getComputerMove = __webpack_require__(4);
-
-var _getComputerMove2 = _interopRequireDefault(_getComputerMove);
-
-var _state = __webpack_require__(15);
-
-var state = _interopRequireWildcard(_state);
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function playGame(e) {
-  if (e.target.innerText === '' && !state.gameOverFlag) {
-    e.target.innerText = state.players[state.currentTurn % 2];
-    if ((0, _isGameOver2.default)(state)) {
-      return (0, _showResult2.default)(state);
-    }
-    state.currentTurn += 1;
-    if (state.currentTurn % 2 === 1) {
-      const computerMove = (0, _getComputerMove2.default)(state);
-      state.board[computerMove].innerText = state.players[state.currentTurn % 2];
-      if ((0, _isGameOver2.default)(state)) {
-        return (0, _showResult2.default)(state);
-      }
-      state.currentTurn += 1;
-    }
-  }
-  return false;
-}
-
-/***/ }),
-/* 7 */,
-/* 8 */,
-/* 9 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _getCornerBox = __webpack_require__(10);
-
-Object.defineProperty(exports, 'getCornerBox', {
-  enumerable: true,
-  get: function () {
-    return _interopRequireDefault(_getCornerBox).default;
-  }
-});
-
-var _getEmptyBox = __webpack_require__(11);
-
-Object.defineProperty(exports, 'getEmptyBox', {
-  enumerable: true,
-  get: function () {
-    return _interopRequireDefault(_getEmptyBox).default;
-  }
-});
-
-var _getForkBlocker = __webpack_require__(12);
-
-Object.defineProperty(exports, 'getForkBlocker', {
-  enumerable: true,
-  get: function () {
-    return _interopRequireDefault(_getForkBlocker).default;
-  }
-});
-
-var _getSideBox = __webpack_require__(13);
-
-Object.defineProperty(exports, 'getSideBox', {
-  enumerable: true,
-  get: function () {
-    return _interopRequireDefault(_getSideBox).default;
-  }
-});
-
-var _isFork = __webpack_require__(14);
-
-Object.defineProperty(exports, 'isFork', {
-  enumerable: true,
-  get: function () {
-    return _interopRequireDefault(_isFork).default;
-  }
-});
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-/***/ }),
-/* 10 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = getCornerBox;
-function getCornerBox(state) {
-  const corners = [0, 2, 6, 8];
-  const temp = [];
-  for (let i = 0; i < 4; i += 1) {
-    if (state.board[corners[i]].innerText === '') {
-      temp.push(corners[i]);
-    }
-  }
-  const randomIndex = Math.floor(Math.random() * temp.length);
-  return temp[randomIndex];
-}
-
-/***/ }),
-/* 11 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = getEmptyBox;
-function getEmptyBox({ board }) {
-  const temp = [];
-  for (let i = 0; i < 9; i += 1) {
-    if (board[i].innerText === '') {
-      temp.push(i);
-    }
-  }
-  const randomIndex = Math.floor(Math.random() * temp.length);
-  return temp[randomIndex];
-}
-
-/***/ }),
-/* 12 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = getForkBlocker;
-function getForkBlocker({ forks, board }) {
-  for (let i = 0; i < 4; i += 1) {
-    const fork = forks[i];
-    if (board[fork[0]].innerText === 'X' && board[fork[1]].innerText === 'X' && board[fork[2]].innerText === '') {
-      return fork[2];
-    }
-  }
-  return false;
-}
-
-/***/ }),
-/* 13 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = getSideBox;
-function getSideBox(state) {
-  const corners = [1, 3, 5, 7];
-  const temp = [];
-  for (let i = 0; i < 4; i += 1) {
-    if (state.board[corners[i]].innerText === '') {
-      temp.push(corners[i]);
-    }
-  }
-  const randomIndex = Math.floor(Math.random() * temp.length);
-  return temp[randomIndex];
-}
-
-/***/ }),
-/* 14 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = isFork;
-function isFork({ board, forks }) {
-  for (let i = 0; i < 4; i += 1) {
-    const fork = forks[i];
-    if (board[fork[0]].innerText === 'X' && board[fork[1]].innerText === 'X' && board[fork[2]].innerText === '') {
-      return true;
-    }
-  }
-  return false;
-}
-
-/***/ }),
-/* 15 */
+/* 2 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -499,7 +174,161 @@ exports.currentTurn = currentTurn;
 exports.winningCondition = winningCondition;
 
 /***/ }),
-/* 16 */
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _game = __webpack_require__(4);
+
+var _state = __webpack_require__(2);
+
+var state = _interopRequireWildcard(_state);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+function main() {
+  state.winner.addEventListener('click', e => {
+    e.target.click = (0, _game.resetGame)(state);
+  });
+  state.board.forEach(square => {
+    square.addEventListener('click', e => {
+      (0, _game.playGame)(e);
+    });
+  });
+}
+
+main();
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _playGame = __webpack_require__(5);
+
+Object.defineProperty(exports, 'playGame', {
+  enumerable: true,
+  get: function () {
+    return _interopRequireDefault(_playGame).default;
+  }
+});
+
+var _isGameOver = __webpack_require__(0);
+
+Object.defineProperty(exports, 'isGameOver', {
+  enumerable: true,
+  get: function () {
+    return _interopRequireDefault(_isGameOver).default;
+  }
+});
+
+var _getComputerMove = __webpack_require__(1);
+
+Object.defineProperty(exports, 'getComputerMove', {
+  enumerable: true,
+  get: function () {
+    return _interopRequireDefault(_getComputerMove).default;
+  }
+});
+
+var _resetGame = __webpack_require__(14);
+
+Object.defineProperty(exports, 'resetGame', {
+  enumerable: true,
+  get: function () {
+    return _interopRequireDefault(_resetGame).default;
+  }
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = playGame;
+
+var _isGameOver = __webpack_require__(0);
+
+var _isGameOver2 = _interopRequireDefault(_isGameOver);
+
+var _showResult = __webpack_require__(6);
+
+var _showResult2 = _interopRequireDefault(_showResult);
+
+var _getComputerMove = __webpack_require__(1);
+
+var _getComputerMove2 = _interopRequireDefault(_getComputerMove);
+
+var _state = __webpack_require__(2);
+
+var state = _interopRequireWildcard(_state);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function playGame(e) {
+  if (e.target.innerText === '' && !state.gameOverFlag) {
+    e.target.innerText = state.players[state.currentTurn % 2];
+    if ((0, _isGameOver2.default)(state)) {
+      return (0, _showResult2.default)(state);
+    }
+    state.currentTurn += 1;
+    if (state.currentTurn % 2 === 1) {
+      const computerMove = (0, _getComputerMove2.default)(state);
+      state.board[computerMove].innerText = state.players[state.currentTurn % 2];
+      if ((0, _isGameOver2.default)(state)) {
+        return (0, _showResult2.default)(state);
+      }
+      state.currentTurn += 1;
+    }
+  }
+  return false;
+}
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = showResult;
+
+var _showWinningSquares = __webpack_require__(7);
+
+var _showWinningSquares2 = _interopRequireDefault(_showWinningSquares);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function showResult(state) {
+  const winnerString = state.currentTurn % 2 === 1 ? 'O is the winner!' : 'This game is a draw!';
+  state.winner.innerHTML = `${winnerString}`;
+  state.winner.style.visibility = 'visible';
+  if (state.gameOverFlag) (0, _showWinningSquares2.default)(state);
+  return false;
+}
+
+/***/ }),
+/* 7 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -517,7 +346,7 @@ function showWinningSquares({ wins, board, winningCondition }) {
 }
 
 /***/ }),
-/* 17 */
+/* 8 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -526,20 +355,188 @@ function showWinningSquares({ wins, board, winningCondition }) {
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = showResult;
 
-var _showWinningSquares = __webpack_require__(16);
+var _getCornerBox = __webpack_require__(9);
 
-var _showWinningSquares2 = _interopRequireDefault(_showWinningSquares);
+Object.defineProperty(exports, 'getCornerBox', {
+  enumerable: true,
+  get: function () {
+    return _interopRequireDefault(_getCornerBox).default;
+  }
+});
+
+var _getEmptyBox = __webpack_require__(10);
+
+Object.defineProperty(exports, 'getEmptyBox', {
+  enumerable: true,
+  get: function () {
+    return _interopRequireDefault(_getEmptyBox).default;
+  }
+});
+
+var _getForkBlocker = __webpack_require__(11);
+
+Object.defineProperty(exports, 'getForkBlocker', {
+  enumerable: true,
+  get: function () {
+    return _interopRequireDefault(_getForkBlocker).default;
+  }
+});
+
+var _getSideBox = __webpack_require__(12);
+
+Object.defineProperty(exports, 'getSideBox', {
+  enumerable: true,
+  get: function () {
+    return _interopRequireDefault(_getSideBox).default;
+  }
+});
+
+var _isFork = __webpack_require__(13);
+
+Object.defineProperty(exports, 'isFork', {
+  enumerable: true,
+  get: function () {
+    return _interopRequireDefault(_isFork).default;
+  }
+});
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function showResult(state) {
-  const winnerString = state.currentTurn % 2 === 1 ? 'O is the winner!' : 'This game is a draw!';
-  state.winner.innerHTML = `${winnerString}`;
-  state.winner.style.visibility = 'visible';
-  if (state.gameOverFlag) (0, _showWinningSquares2.default)(state);
+/***/ }),
+/* 9 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = getCornerBox;
+function getCornerBox(state) {
+  const corners = [0, 2, 6, 8];
+  const temp = [];
+  for (let i = 0; i < 4; i += 1) {
+    if (state.board[corners[i]].innerText === '') {
+      temp.push(corners[i]);
+    }
+  }
+  const randomIndex = Math.floor(Math.random() * temp.length);
+  return temp[randomIndex];
+}
+
+/***/ }),
+/* 10 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = getEmptyBox;
+function getEmptyBox({ board }) {
+  const temp = [];
+  for (let i = 0; i < 9; i += 1) {
+    if (board[i].innerText === '') {
+      temp.push(i);
+    }
+  }
+  const randomIndex = Math.floor(Math.random() * temp.length);
+  return temp[randomIndex];
+}
+
+/***/ }),
+/* 11 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = getForkBlocker;
+function getForkBlocker({ forks, board }) {
+  for (let i = 0; i < 4; i += 1) {
+    const fork = forks[i];
+    if (board[fork[0]].innerText === 'X' && board[fork[1]].innerText === 'X' && board[fork[2]].innerText === '') {
+      return fork[2];
+    }
+  }
   return false;
+}
+
+/***/ }),
+/* 12 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = getSideBox;
+function getSideBox(state) {
+  const corners = [1, 3, 5, 7];
+  const temp = [];
+  for (let i = 0; i < 4; i += 1) {
+    if (state.board[corners[i]].innerText === '') {
+      temp.push(corners[i]);
+    }
+  }
+  const randomIndex = Math.floor(Math.random() * temp.length);
+  return temp[randomIndex];
+}
+
+/***/ }),
+/* 13 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = isFork;
+function isFork({ board, forks }) {
+  for (let i = 0; i < 4; i += 1) {
+    const fork = forks[i];
+    if (board[fork[0]].innerText === 'X' && board[fork[1]].innerText === 'X' && board[fork[2]].innerText === '') {
+      return true;
+    }
+  }
+  return false;
+}
+
+/***/ }),
+/* 14 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = resetGame;
+function resetGame(state) {
+  state.currentTurn = 0;
+  state.winner.style.visibility = 'hidden';
+  state.board.forEach(square => {
+    const temp = square;
+    temp.innerText = '';
+  });
+  const winningSquares = document.querySelectorAll('div.square');
+  for (let i = 0; i < winningSquares.length; i += 1) {
+    winningSquares[i].classList.remove(`win-${state.winningCondition}`);
+  }
+  state.winningCondition = -1;
+  state.gameOverFlag = false;
 }
 
 /***/ })
